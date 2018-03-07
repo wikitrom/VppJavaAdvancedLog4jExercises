aa6117acb1c48cdda7628b0d3080f54f1754e1e8
Mats Bergstroem <hackingtrom@gmail.com>
2018-03-07 09:47:22 +0100

    Add class specific logger

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ import org.apache.logging.log4j.Logger;
 public class Main {
 
 	static Logger logger = LogManager.getLogger();
-	
+
 	public static void main(String[] args) {
 
 		logger.log(Level.ERROR, "Fake error!");
 		AnotherClass ac = new AnotherClass();
 		ac.testMethod();
+		logger.trace("A TRACE event");
+		logger.info("Another fake INFO event");
 	}
 
 }
